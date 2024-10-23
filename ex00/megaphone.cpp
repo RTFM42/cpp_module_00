cc47e8d8bdab6d9a72bff6047b4f19918cde3892
yush <yus-sato@kalyte.ro>
2024-10-23 20:47:17 +0900

    Update: fmt

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: yussato <yussato@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 17:30:36 by yussato           #+#    #+#             */
-/*   Updated: 2024/10/23 20:15:27 by yussato          ###   ########.fr       */
+/*   Updated: 2024/10/23 20:42:17 by yussato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,18 @@
 
 int main(int ac, char **av)
 {
-    int n;
-    int i;
+	int n;
+	int i;
 
-    n = -1;
-    if (ac == 1)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-    while (1 < ac && av && av[++n])
-    {
-        i = 0;
-        while (n > 0 && av[n][i])
-            std::cout << (char)std::toupper(av[n][i++]);
-    }
-    std::cout << std::endl;
-    return (0);
+	n = -1;
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while (1 < ac && av && av[++n])
+	{
+		i = 0;
+		while (n > 0 && av[n][i])
+			std::cout << (char)std::toupper(av[n][i++]);
+	}
+	std::cout << std::endl;
+	return (0);
 }
