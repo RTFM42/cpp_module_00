f0994d09a59aca4700d45fd8dc6a3c6be01cbb0c
yush <yus-sato@kalyte.ro>
2024-11-05 20:53:13 +0000

    Fix: segv & char to string

--- ex01/PhoneBook.cpp
+++ ex01/PhoneBook.cpp
@@ -15,6 +15,7 @@
 
 PhoneBook::PhoneBook(void)
 {
+	this->__index__ = 0;
 }
 
 PhoneBook::~PhoneBook(void)
@@ -23,49 +24,46 @@ PhoneBook::~PhoneBook(void)
 
 void PhoneBook::__print__(int index)
 {
-	if (!this->__contacts__[index].getFname().size()
-		&& std::cout << "There is no contact at this index." << std::endl)
-		return ;
-	
+	if (!this->__contacts__[index].getFname().size() && std::cout << "There is no contact at this index." << std::endl)
+		return;
+
 	std::cout << "first name: "
-		<< this->__contacts__[index].getFname() << std::endl;
+			  << this->__contacts__[index].getFname() << std::endl;
 	std::cout << "last name: "
-		<< this->__contacts__[index].getLname() << std::endl;
+			  << this->__contacts__[index].getLname() << std::endl;
 	std::cout << "nickname: "
-		<< this->__contacts__[index].getNname() << std::endl;
+			  << this->__contacts__[index].getNname() << std::endl;
 	std::cout << "phone number: "
-		<< this->__contacts__[index].getPhone() << std::endl;
+			  << this->__contacts__[index].getPhone() << std::endl;
 	std::cout << "darkest secret: "
-		<< this->__contacts__[index].getSecret() << std::endl;
+			  << this->__contacts__[index].getSecret() << std::endl;
 }
 
 void PhoneBook::add(void)
 {
 	std::string str;
 
-	if (this->__index__ == 8)
-		this->__index__ = 0;
-	this->__index__++;
 	str = "";
 	while (!std::cin.eof() && str == "" && std::cout << "ADD> first name>")
 		if (std::getline(std::cin, str) && str != "")
-			this->__contacts__[this->__index__].setFname(str);
+			this->__contacts__[this->__index__ % 8].setFname(str);
 	str = "";
 	while (!std::cin.eof() && str == "" && std::cout << "ADD> last name>")
 		if (std::getline(std::cin, str) && str != "")
-			this->__contacts__[this->__index__].setLname(str);
+			this->__contacts__[this->__index__ % 8].setLname(str);
 	str = "";
 	while (!std::cin.eof() && str == "" && std::cout << "ADD> nickname>")
 		if (std::getline(std::cin, str) && str != "")
-			this->__contacts__[this->__index__].setNname(str);
+			this->__contacts__[this->__index__ % 8].setNname(str);
 	str = "";
 	while (!std::cin.eof() && str == "" && std::cout << "ADD> phone number>")
 		if (std::getline(std::cin, str) && str != "")
-			this->__contacts__[this->__index__].setPhone(str);
+			this->__contacts__[this->__index__ % 8].setPhone(str);
 	str = "";
 	while (!std::cin.eof() && str == "" && std::cout << "ADD> darkest secret>")
 		if (std::getline(std::cin, str) && str != "")
-			this->__contacts__[this->__index__].setSecret(str);
+			this->__contacts__[this->__index__ % 8].setSecret(str);
+	this->__index__++;
 }
 
 std::string fix10(std::string str)
@@ -83,19 +81,18 @@ std::string fix10(std::string str)
 
 void PhoneBook::search(void)
 {
-	int			index;
+	int index;
 	std::string index_str;
 
 	index = 0;
-	if (this->__index__ == 0
-		&& std::cout << "There is no contact." << std::endl)
-		return ;
-	while (++index <= this->__index__)
+	if (this->__index__ == 0 && std::cout << "There is no contact." << std::endl)
+		return;
+	while (++index <= this->__index__ && index <= 8)
 	{
-		std::cout << "|" << fix10("" + (index + '0'));
-		std::cout << "|" << fix10(this->__contacts__[index].getFname());
-		std::cout << "|" << fix10(this->__contacts__[index].getLname());
-		std::cout << "|" << fix10(this->__contacts__[index].getNname());
+		std::cout << "|" << fix10(std::string(1, index + '0'));
+		std::cout << "|" << fix10(this->__contacts__[index - 1].getFname());
+		std::cout << "|" << fix10(this->__contacts__[index - 1].getLname());
+		std::cout << "|" << fix10(this->__contacts__[index - 1].getNname());
 		std::cout << "|" << std::endl;
 	}
 	while (!std::cin.eof())
@@ -103,12 +100,11 @@ void PhoneBook::search(void)
 		std::cout << "SEARCH> index>";
 		if (std::getline(std::cin, index_str), index_str != "")
 		{
-			if (index_str.size() == 1
-				&& '1' <= index_str[0] && index_str[0] <= '8')
+			if (index_str.size() == 1 && '1' <= index_str[0] && index_str[0] <= '8')
 			{
 				index = index_str[0] - '0';
-				this->__print__(index);
-				break ;
+				this->__print__(index - 1);
+				break;
 			}
 		}
 		if (index_str != "")
